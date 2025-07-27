aa28bc80f71580c63a15a27d79c8734b8bff2df2
Alex Patterson <alex@codingcat.dev>
2025-07-27 18:26:35 -0400

    fix process.env missing

--- complete-application/app.js
+++ complete-application/app.js
@@ -57,7 +57,7 @@ passport.use(
       tokenURL: `${FUSIONAUTH_URL}/oauth2/token`,
       clientID: FUSIONAUTH_APP_CLIENTID,
       clientSecret: FUSIONAUTH_APP_CLIENT_SECRET,
-      callbackURL: VERCEL_PROJECT_PRODUCTION_URL ? `https://${VERCEL_PROJECT_PRODUCTION_URL}/auth/callback` : "http://localhost:3000/auth/callback",
+      callbackURL: process?.env?.VERCEL_PROJECT_PRODUCTION_URL ? `https://${VERCEL_PROJECT_PRODUCTION_URL}/auth/callback` : "http://localhost:3000/auth/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       // Get the user profile from Fusion:
